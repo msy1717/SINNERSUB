e5961e9e3fe61d54f94ba1f105099960360a97bc
msy1717 <80758264+msy1717@users.noreply.github.com>
2021-09-11 13:27:34 +0530

    Update alive.py

--- userbot/modules/alive.py
+++ userbot/modules/alive.py
@@ -133,12 +133,12 @@ async def amireallyalive(alive):
     """For .alive command, check if the bot is running."""
     logo = ALIVE_LOGO
     output = (
-        f"*The SinX Userbot...\n\nHey, I am alive.*{repo.active_branch.name}`\n"
-        "*====================================*\n"
-        f"*🐍 Python *        :` v{python_version()}\n"
-        f"*⚙️ Telethon*       :` v{version.__version__}\n"
-        f"*👤 User *          :` {DEFAULTUSER}\n"         
-        "*====================================*\n"
+        f"The SinX Userbot...\n\nHey, I am alive. {repo.active_branch.name}`\n"
+        "====================================\n"
+        f"🐍 Python       :` v{python_version()}\n"
+        f"⚙️ Telethon    :` v{version.__version__}\n"
+        f"👤 User        :` {DEFAULTUSER}\n"         
+        "====================================\n"
     )
     if ALIVE_LOGO:
         try:
